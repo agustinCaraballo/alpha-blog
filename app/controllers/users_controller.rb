2fc025e624c637b05408612ec8d551d6d8f9b857
Agustin Caraballo <agustincc@gmail.com>
2017-05-03 21:42:21 -0500

    Completed the auth system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 before_action :set_user, only: [:edit,:show,:update,:destroy]
+before_action :require_same_user, only: [:edit,:update]
 def new
   @user = User.new()
 end
@@ -38,5 +39,14 @@ def set_user
 end
 def user_params
   params.required(:user).permit(:username,:email,:password)
+end
+def require_same_user
+  if logged_in? and current_user != @user
+    flash[:danger] = "You can only edit your own account"
+    redirect_to root_path
+  elsif !logged_in?
+    flash[:danger] = "You need to be logged in to perform that action"
+    redirect_to signup_path
+  end
 end
 end
