34308deec4ac521f73cd4756e9994b3c9ff7f77f
Agustin Caraballo <agustincc@gmail.com>
2017-05-10 22:46:19 -0500

    Added test for require admin for new categories

--- test/controllers/categoties_controller_test.rb
+++ test/controllers/categoties_controller_test.rb
@@ -2,12 +2,14 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com",password: "password",admin: true)
   end
   test "should get categories index" do
     get "/categories/index"
     assert_response :success
   end
   test "should get new" do
+    post login_path,params: {session:{email: @user.email,password: @user.password}}
     get "/categories/new"
     assert_response :success
   end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
   class CreateCategoriesTest < ActionDispatch::IntegrationTest
+    def setup
+      @user = User.create(username: "john", email: "john@example.com",password: "password",admin: true)
+    end
   test "get new categories form and create category" do
+    post login_path,params: {session:{email: @user.email,password: @user.password}}
     get "/categories/new"
     assert_response :success
     assert_difference 'Category.count',1 do
@@ -11,6 +15,7 @@ require 'test_helper'
     assert_match "sports", response.body
   end
   test "invalid category submission results in failing" do
+    post login_path,params: {session:{email: @user.email,password: @user.password}}
     get "/categories/new"
     assert_response :success
     assert_no_difference 'Category.count' do
@@ -19,5 +24,11 @@ require 'test_helper'
     assert_response :success
     assert_select "h2.panel-title"
     assert_select "div.panel-body"
+  end
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category:{name: "sports"}}
+    end
+    assert_redirected_to categories_path
   end
 end
