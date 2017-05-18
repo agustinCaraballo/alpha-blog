fa4a284f09c3b2afaf1e45b4e875117ccdb58c7f
Agustin Caraballo <agustincc@gmail.com>
2017-05-18 18:22:44 -0500

    Added categories edit functions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,6 +22,20 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page],per_page: 5)
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Name was succesfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def category_params
     params.require(:category).permit(:name)
